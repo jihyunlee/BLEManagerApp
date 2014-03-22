63b50c5272bf2263cfe7be7493b914b973061987
Andy Sigler <andrewsigler1@gmail.com>
2014-03-22 14:28:31 -0400

    test

--- www/js/index.js
+++ www/js/index.js
@@ -11,7 +11,8 @@ bind: function() {
     colorScreen.hidden = true;
 },
 deviceready: function() {
-    
+    console.log("deviceready");
+    console.log("deviceready2");
     if(window.cordova.logger) {
         window.cordova.logger.__onDeviceReady();
     }
